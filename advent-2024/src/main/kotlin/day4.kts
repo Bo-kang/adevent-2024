316014e41c75befdad3290dad28898c039dbcba7
Bo-kang <ebfks0301@gmail.com>
2024-12-05 14:01:15 +0900

    feat: day4 part two

--- advent-2024/src/main/kotlin/day4.kts
+++ advent-2024/src/main/kotlin/day4.kts
@@ -1,6 +1,10 @@
 object Day4{
     const val TARGET_WORD = "XMAS"
     const val BEGIN_LETTER = 'X'
+
+    const val TARGET_WORD2 = "MAS"
+    const val BEGIN_LETTER2 = 'M'
+
     enum class Direction(val i: Int, val j: Int) {
         U(-1, 0),
         UR(-1, +1),
@@ -12,26 +16,27 @@ object Day4{
         UL(-1, -1);
     }
 
-    fun List<String>.isAvailDirection(dir: Direction, amount: Int = 1, curI: Int, curJ: Int): Boolean {
+    fun List<String>.isAvailDirection(dir: Direction,  curI: Int, curJ: Int, amount: Int = 1 ): Boolean {
         return (curI + dir.i * amount) >= 0
                 && (curI + dir.i * amount) < this.size
                 && (curJ + dir.j * amount) >= 0
                 && (curJ + dir.j * amount) < this.first().length
     }
 
-    fun isAbleToMakeWord(dir: Direction, input: List<String>, curI: Int, curJ: Int): Boolean {
-        if(!input.isAvailDirection(dir, TARGET_WORD.length - 1, curI, curJ)){
+    fun isAbleToMakeWord(dir: Direction, input: List<String>, curI: Int, curJ: Int, targetWord: String = TARGET_WORD): Boolean {
+        if(!input.isAvailDirection(dir, curI, curJ, targetWord.length - 1)){
             return false
         }
 
-        for(i in 0 until TARGET_WORD.length){
-            if(input[curI + (dir.i * i)][curJ + (dir.j * i)] != TARGET_WORD[i]){
+        for(i in targetWord.indices){
+            if(input[curI + (dir.i * i)][curJ + (dir.j * i)] != targetWord[i]){
                 return false
             }
         }
 
         return true
     }
+
 
     fun partOneSolution(input: List<String>): Int{
         var answer = 0
@@ -48,9 +53,37 @@ object Day4{
         }
         return answer
     }
+
+    fun partTwoSolution(input: List<String>): Int{
+        val tmp = mutableMapOf<Pair<Int, Int>, Int>()
+
+        var answer = 0
+        input.forEachIndexed { i, it ->
+            it.forEachIndexed { j, letter ->
+                if(letter == BEGIN_LETTER2){
+                    Direction.entries.forEach { direction ->
+                        if(direction.name.length > 1) {
+                            if (isAbleToMakeWord(direction, input, i, j, TARGET_WORD2)){
+                                if(tmp.contains(i+direction.i to j+direction.j))
+                                    tmp[i+direction.i to j+direction.j] = 2
+                                else
+                                    tmp[i+direction.i to j+direction.j] = 1
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+        tmp.forEach { (_, count) ->
+            if(count == 2) answer++
+        }
+
+        return answer
+
+    }
 }
-
-
 
 
 val testInput = listOf(
@@ -67,4 +100,5 @@ val testInput = listOf(
 )
 
 println(Day4.partOneSolution(testInput))
+println(Day4.partTwoSolution(testInput))
 
